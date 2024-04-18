43c6a8f8262e5d42c8fe02b32fb650362e91cf81
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-04-18 15:42:21 +0530

    New Changes

---
+++ src/JavaBasicCode/HelloWorld.java
@@ -0,0 +1,12 @@
+package JavaBasicCode;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Enter Name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
