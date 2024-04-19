89869526d13c48999b1aaaa05c6ad500bf5dd653
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-04-19 16:23:20 +0530

    Count Even and Odd Digite Number

---
+++ src/JavaBasicCode/CountEvenandOddDigituinNumber.java
@@ -0,0 +1,25 @@
+package JavaBasicCode;
+
+import java.util.Scanner;
+
+public class CountEvenandOddDigituinNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number ");
+        int num = sc.nextInt();
+        int even = 0;
+        int odd = 0;
+        int rem = 0;
+         while(num>0){
+             rem = num % 10;
+             if(rem%2==0){
+                 even++;
+             }else {
+                 odd++;
+             }
+             num = num/10;
+
+         }
+        System.out.println("odd :-"+odd+" ::: " +"even:-"+ even);
+    }
+}
