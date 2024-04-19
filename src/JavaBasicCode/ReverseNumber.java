aceb09358380070192bb651746a89f51ec6906a2
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-04-19 11:00:15 +0530

    ReverseNumber using multiple ways

--- src/JavaBasicCode/ReverseNumber.java
+++ src/JavaBasicCode/ReverseNumber.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class ReverseNumber {
 
-    public static void usingAlgo(int num){
+    public static void usingAlgo(int num) {
         int rev = 0;
-        while(num!=0){
-            rev = rev*10 + num%10;
-            num = num/10;
+        while (num != 0) {
+            rev = (rev * 10) + (num % 10);
+            num = num / 10;
         }
-        System.out.println("Reverse Number is "+rev);
+        System.out.println("Reverse Number is " + rev);
+    }
+
+    public static void usingStringBufferClass(int num) {
+    StringBuffer rev;
+    StringBuffer sb  =  new StringBuffer(String.valueOf(num));
+    rev = sb.reverse();
+    System.out.println("Reverse Number using String Buffer class " + rev);
+
     }
 
     public static void main(String[] args) {
@@ -18,7 +26,9 @@ public class ReverseNumber {
         System.out.println("Enter the Number :-");
         int num = sc.nextInt();
         // using ALgorithum
-        usingAlgo(num);
+//        usingAlgo(num);
+        // using StringBuffer class
+        usingStringBufferClass(num);
 
     }
 }
