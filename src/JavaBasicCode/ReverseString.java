bf26ca0818a8187fc5166c764b24bf74cb0a23b2
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-04-19 14:08:42 +0530

    Reverse String using multiple ways

---
+++ src/JavaBasicCode/ReverseString.java
@@ -0,0 +1,35 @@
+package JavaBasicCode;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void usingAlgorithum(String value) {
+        char[] newValue = value.toCharArray();
+        String revString = "";
+        for (int i = newValue.length - 1; i >= 0; i--) {
+            revString += newValue[i];
+        }
+        System.out.println("using Alogrithum :-"+revString);
+    }
+
+    public static void usingStringBuffer(String value) {
+        StringBuffer sb = new StringBuffer(value);
+        System.out.println("using stringBuffer :-" + sb.reverse());
+    }
+
+    public static void usingStringBuilder(String value) {
+        StringBuilder sb1 = new StringBuilder(value);
+        System.out.println("using StringBuilder :-" + sb1.reverse());
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String :-");
+        String value = sc.nextLine(); // Use nextLine() to read the entire line
+        usingAlgorithum(value);
+        usingStringBuffer(value);
+        usingStringBuilder(value);
+    }
+}
