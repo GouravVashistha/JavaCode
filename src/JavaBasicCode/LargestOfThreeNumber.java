b7250bc4e02b8434553816c1daad1130558b5bd2
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-04-21 23:30:36 +0530

    LargestOfThreeNumber

---
+++ src/JavaBasicCode/LargestOfThreeNumber.java
@@ -0,0 +1,18 @@
+package JavaBasicCode;
+
+public class LargestOfThreeNumber {
+    public static void main(String[] args) {
+        int a = 4, b = 1, c = 31;
+
+        if (a > b && a > c) {
+            System.out.println("a is Greater");
+        } else if (b > c && b > a) {
+            System.out.println("b is greater");
+        } else {
+            System.out.println("c is greater");
+        }
+
+        int variable = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println("max :"+ variable);
+    }
+}
