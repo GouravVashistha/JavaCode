d3c2f84df80101749ce320b774235bfd7fee3bdb
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-04-24 20:07:58 +0530

    Inhertenche Implementation Example

--- src/JavaBasicCode/InhertencheExample.java
+++ src/JavaBasicCode/InhertencheExample.java
@@ -1,5 +1,16 @@
 package JavaBasicCode;
+/*
+    Inheritance is a machanism by which one object acquires all the properties and Behaviou
+    of another object of another class. It is use for code reusebility and Method Overriding
 
+    There is 5 type of Inheritance In java:
+        1. SIngle level
+        2. Multi level
+        3. Huybrid Inheritance
+        4. Hierarchical Inheritance
+
+    java Dose not suport Multiple inhertance...
+ */
 class Animal{
     void Sound(){
         System.out.println("Animal Make Sound...!");
@@ -8,9 +19,16 @@ class Animal{
 class Dog extends Animal{
     @Override
     void Sound() {
-        super.Sound();
+        System.out.println("Dog Barks...");
     }
+
 }
 public class InhertencheExample {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        animal.Sound();
+        dog.Sound();
+    }
 
 }
