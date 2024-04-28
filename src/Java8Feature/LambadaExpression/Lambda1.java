e016f1147fe758ccb753cf86840e88a9642d8b04
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-04-28 16:40:21 +0530

    Lambda and Functional Interface

--- src/Java8Feature/LambadaExpression/Lambda1.java
+++ src/Java8Feature/LambadaExpression/Lambda1.java
@@ -3,13 +3,21 @@ package Java8Feature.LambadaExpression;
 public abstract class Lambda1 implements MyInter{
     public static void main(String[] args) {
         System.out.println("MY system start..!");
+//      for running  blow code amke class abstract
+//        MyInter i = new MyInter() {
+//            @Override
+//            public void sayHello() {
+//                System.out.println("Hello gaurav this side..");
+//            }
+//        };
+//        i.sayHello();
 
-        MyInter i = new MyInter() {
-            @Override
-            public void sayHello() {
-                System.out.println("Hello gaurav this side..");
-            }
+        // using our interface with the help of lambda
+
+        MyInter i = ()->{
+            System.out.println("this is first lambda program");
         };
         i.sayHello();
+
     }
 }
