9fa62bbc094c91b0b10f1584a2824f7c9697bd1e
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-02 19:42:36 +0530

    FibonacciNumber

--- src/DSA/Recursion/FibonacciNumber.java
+++ src/DSA/Recursion/FibonacciNumber.java
@@ -1,7 +1,24 @@
 package DSA.Recursion;
 
 public class FibonacciNumber {
+    public static void withOutRecursion(int count){
+        int a = 0;
+        int b = 1;
+        System.out.print(a+" "+b);
+        for(int i=2;i<count;i++){
+            int c = a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;
+        }
+    }
+
+    public static void withRecursion(int count){
+
+    }
     public static void main(String[] args) {
+        int count  = 10;
+        withOutRecursion(count);
 
     }
 }
