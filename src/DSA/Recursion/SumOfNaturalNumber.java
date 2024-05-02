8781ef24f89eb84604485af054ed46fc1f635a85
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-02 18:12:30 +0530

    SumOfNaturalNumber

--- src/DSA/Recursion/SumOfNaturalNumber.java
+++ src/DSA/Recursion/SumOfNaturalNumber.java
@@ -1,8 +1,18 @@
 package DSA.Recursion;
 
 public class SumOfNaturalNumber {
+    public static void NaturalNumber(int i,int n ,int sum){
+        if(i == n){
+            sum +=i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        NaturalNumber(i+1,n,sum);
 
+    }
     public static void main(String[] args) {
 
+    NaturalNumber(1, 5, 0);
     }
 }
