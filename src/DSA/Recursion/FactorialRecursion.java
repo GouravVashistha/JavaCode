cda2b8d6769816f18e698fcf897df6544846e7cb
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-02 19:32:13 +0530

    FactorialRecursion

--- src/DSA/Recursion/FactorialRecursion.java
+++ src/DSA/Recursion/FactorialRecursion.java
@@ -14,6 +14,18 @@ public class FactorialRecursion {
         return  n*factorial(n-1);
     }
 
+    public static int calcFactorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+
+        int fact_nm1 = calcFactorial(n-1);
+        int fact_n = n*fact_nm1;
+        return fact_n;
+    }
+
+
+
     public static void main(String[] args) {
         FactorialRecursion f = new FactorialRecursion();
         Scanner sc = new Scanner(System.in);
@@ -22,5 +34,6 @@ public class FactorialRecursion {
         var facto = f.factorial(num);
         System.out.println(facto);
 
+        calcFactorial(num);
     }
 }
