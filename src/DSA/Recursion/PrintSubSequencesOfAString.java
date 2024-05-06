59305468d16a6919fe061e7ade183f0be8dc8ecc
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-06 23:41:04 +0530

    PrintSubSequencesOfAString

--- src/DSA/Recursion/PrintSubSequencesOfAString.java
+++ src/DSA/Recursion/PrintSubSequencesOfAString.java
@@ -1,7 +1,19 @@
 package DSA.Recursion;
 
 public class PrintSubSequencesOfAString {
+    public static  void subSequences(String str,int idx,String newString){
+        if(idx ==str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar =  str.charAt(idx);
+        // to be
+        subSequences(str, idx+1, newString+currChar);
+        // or not to be
+        subSequences(str, idx+1, newString);
+    }
     public static void main(String[] args) {
-
+        String str = "abc";
+        subSequences(str, 0, "");
     }
 }
