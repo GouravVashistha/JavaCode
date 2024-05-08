6de2397c75fdfc7b837302f1fde7b8969c117c24
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-08 16:27:24 +0530

    how to array insilization

--- src/DSA/Arrays/ArrayDemo1.java
+++ src/DSA/Arrays/ArrayDemo1.java
@@ -10,5 +10,9 @@ public class ArrayDemo1 {
         intArray[1] = 2;
         intArray[2] = 3;
         System.out.println(Arrays.toString(intArray));
+
+        String sArray[] = {"a","b","c"};
+        System.out.println(Arrays.toString(sArray));
+
     }
 }
