5b9fbc0a9982faf49ed6295abd164759813ee339
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-10 20:39:49 +0530

    TwoDimensionalArray Inserting the value

--- src/DSA/Arrays/TwoDimensionalArray.java
+++ src/DSA/Arrays/TwoDimensionalArray.java
@@ -1,5 +1,7 @@
 package DSA.Arrays;
 
+import java.util.Arrays;
+
 public class TwoDimensionalArray {
     int arr[][] = null;
     // Constructor
@@ -12,7 +14,33 @@ public class TwoDimensionalArray {
         }
     }
 
+    public void insertValueInTheArray(int row,int col,int value){
+        try{
+           if(arr[row][col] == Integer.MIN_VALUE){
+               arr[row][col] =value;
+               System.out.println("successfully Inserted");
+           }else{
+               System.out.println("This cell is already occupied By: "+arr[row][col]);
+           }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid index to access array!");
+        }
+    }
+
+
+
     public static void main(String[] args) {
+        TwoDimensionalArray tda = new TwoDimensionalArray(3, 3);
+        tda.insertValueInTheArray(0, 0, 10);
+        tda.insertValueInTheArray(0, 1, 20);
+        tda.insertValueInTheArray(0, 2, 20);
+        tda.insertValueInTheArray(1, 0, 30);
+        tda.insertValueInTheArray(0, 0, 40);
+        tda.insertValueInTheArray(1, 1, 50);
+        tda.insertValueInTheArray(2, 0, 60);
+        tda.insertValueInTheArray(2, 1, 70);
+        tda.insertValueInTheArray(2, 2, 80);
+        System.out.println(Arrays.deepToString(tda.arr));
 
     }
 }
