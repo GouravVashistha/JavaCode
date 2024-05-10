e0c6903d40cb205aa92dab3054b028c43ddc2637
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-10 20:49:26 +0530

    TwoDimensionalArray

--- src/DSA/Arrays/TwoDimensionalArray.java
+++ src/DSA/Arrays/TwoDimensionalArray.java
@@ -4,29 +4,47 @@ import java.util.Arrays;
 
 public class TwoDimensionalArray {
     int arr[][] = null;
+
     // Constructor
     public TwoDimensionalArray(int numberOfRows, int numberOfColumns) {
         this.arr = new int[numberOfRows][numberOfColumns];
         for (int row = 0; row < arr.length; row++) {
-            for(int col = 0; col< arr[0].length; col++){
+            for (int col = 0; col < arr[0].length; col++) {
                 arr[row][col] = Integer.MIN_VALUE;
             }
         }
     }
 
-    public void insertValueInTheArray(int row,int col,int value){
-        try{
-           if(arr[row][col] == Integer.MIN_VALUE){
-               arr[row][col] =value;
-               System.out.println("successfully Inserted");
-           }else{
-               System.out.println("This cell is already occupied By: "+arr[row][col]);
-           }
-        }catch (ArrayIndexOutOfBoundsException e){
+    public void insertValueInTheArray(int row, int col, int value) {
+        try {
+            if (arr[row][col] == Integer.MIN_VALUE) {
+                arr[row][col] = value;
+                System.out.println("successfully Inserted");
+            } else {
+                System.out.println("This cell is already occupied By: " + arr[row][col]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid index to access array!");
         }
     }
 
+    public void printElement() {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void accessCell(int row, int col) {
+        System.out.println("\nAccessing Row# " + row + " col# " + col);
+        try{
+            System.out.println("Cell value is :"+arr[row][col]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid Index for 2D array");
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -41,6 +59,8 @@ public class TwoDimensionalArray {
         tda.insertValueInTheArray(2, 1, 70);
         tda.insertValueInTheArray(2, 2, 80);
         System.out.println(Arrays.deepToString(tda.arr));
-
+        System.out.println("Print The 2D Array:-");
+        tda.printElement();
+        tda.accessCell(0, 1);
     }
 }
