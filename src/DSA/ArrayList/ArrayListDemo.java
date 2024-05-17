d4b42789315907e2631623903b240eb80ab8ee89
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-17 21:33:15 +0530

    ArrayListDemo create array , insert data  , accessing the element

--- src/DSA/ArrayList/ArrayListDemo.java
+++ src/DSA/ArrayList/ArrayListDemo.java
@@ -1,7 +1,15 @@
 package DSA.ArrayList;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
     public static void main(String[] args) {
-
+        ArrayList<Integer> newArr = new ArrayList<>();
+        newArr.add(1);
+        newArr.add(2);
+        newArr.addFirst(3);
+        newArr.addLast(4);
+        newArr.contains(3);
+        System.out.println(newArr);
     }
 }
