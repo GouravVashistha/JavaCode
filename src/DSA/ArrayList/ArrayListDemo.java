aacd994f11e9185618b41d4e6c1c6026598895c5
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-18 15:57:29 +0530

    ArrayListDemo create array , insert data  , accessing the element

--- src/DSA/ArrayList/ArrayListDemo.java
+++ src/DSA/ArrayList/ArrayListDemo.java
@@ -1,6 +1,8 @@
 package DSA.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
@@ -11,5 +13,22 @@ public class ArrayListDemo {
         newArr.addLast(4);
         newArr.contains(3);
         System.out.println(newArr);
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("Gaurav","jatin","Shabbir"));
+        System.out.println(names);
+        // iterat using for loop
+        for(int i = 0;i<newArr.size();i++){
+            int num = newArr.get(i);
+            System.out.println(num);
+        }
+        // using for each loop
+        for(String name:names){
+            System.out.println(name);
+        }
+
+        Iterator<String> itr = names.iterator();
+        while (itr.hasNext()){
+            String letter = itr.next();
+            System.out.println(letter);
+        }
     }
 }
