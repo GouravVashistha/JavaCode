a996faa42c6f631646a2283c35d863f3bf28d4f9
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-06-03 02:49:32 +0530

    test

--- src/Java8Feature/MethodRefernce/Javatest.java
+++ src/Java8Feature/MethodRefernce/Javatest.java
@@ -2,6 +2,6 @@ package Java8Feature.MethodRefernce;
 
 public class Javatest {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("garuav");
     }
 }
