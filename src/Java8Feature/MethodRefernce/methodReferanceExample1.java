a466d4005a952cbe07eb2f0781ac32c9428311a2
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-06-03 03:25:19 +0530

    Method Reference

--- src/Java8Feature/MethodRefernce/methodReferanceExample1.java
+++ src/Java8Feature/MethodRefernce/methodReferanceExample1.java
@@ -1,7 +1,8 @@
 package Java8Feature.MethodRefernce;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Person{
     private String name;
@@ -18,15 +19,49 @@ class Person{
         return name;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
-public class methodReferanceExample1 {
+public class methodReferanceExample1{
+
+    public static int compareByName(Person a, Person b){
+        return a.getName().compareTo(b.getName());
+    }
+    public static int compareByAge(Person a, Person b){
+        return a.getAge().compareTo(b.getAge());
+    }
+
+
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
-        personList.add(new Person("Gaurav", 24));
+        personList.add(new Person("Gaurav", 57));
         personList.add(new Person("Shabbir",55));
         personList.add(new Person("Yash", 5));
         personList.add(new Person("Sidharth",105));
-        System.out.println(personList);
+        /*
+        Trying to Print the Data using multiple way..
+         */
+//        personList.forEach(person -> System.out.println(person));
+
+//        personList.forEach(System.out::println);
+
+        Iterator<Person> itr= personList.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+
+        Collections.sort(personList,methodReferanceExample1::compareByName);
+        System.out.println("Filter By Name :");
+        personList.stream().map(x -> x.getName()).forEach(System.out::println);
+        System.out.println("Filter By Age :");
+        Collections.sort(personList,methodReferanceExample1::compareByAge);
+        personList.stream().map(x->x.getAge()).forEach(System.out::println);
 
     }
 
