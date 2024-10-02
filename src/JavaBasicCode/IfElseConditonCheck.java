435da63de96d88e8ef08d8e80d66905c1ae4af68
Gourav vashistha <gvashistha4@gmail.com>
2024-10-03 02:05:18 +0530

    ifelse

--- src/JavaBasicCode/IfElseConditonCheck.java
+++ src/JavaBasicCode/IfElseConditonCheck.java
@@ -19,12 +19,25 @@ public class IfElseConditonCheck {
             System.out.println("you are not adult..");
         }
     }
+    public void checkgreaterOrLess(int a,int b){
+        if(a==b){
+            System.out.println("equal");
+        }else if(a>b){
+            System.out.println("a is greater");
+
+        }else{
+            System.out.println("b is greater");
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         // Vote(n);
-        checkOdd(n);
-
+        // checkOdd(a);
+        IfElseConditonCheck check = new IfElseConditonCheck();
+        check.checkgreaterOrLess(a,b);
         sc.close();
     }
 }
