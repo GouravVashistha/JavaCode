f68a7296a3ff7dcb56cf10993187042636b4989d
Gourav vashistha <gvashistha4@gmail.com>
2024-10-02 21:43:47 +0530

    BASIC

--- src/JavaBasicCode/JavaBasicStart.java
+++ src/JavaBasicCode/JavaBasicStart.java
@@ -1,24 +1,34 @@
 package JavaBasicCode;
 
+import java.util.Scanner;
+
 public class JavaBasicStart {
 
-    public static void takeInput(){
+    public static void takeInput() {
         System.out.println("In this Method we can check \n how to take Input form the user..!!");
     }
+    
 
-    protected static void sumofNumber(int a,int b){
+    protected static void sumofNumber(int a, int b) {
         // (* / %) > (+ -)
-        int sum = a+b;
+
+        int sum = a + b;
         System.out.println(sum);
-        int ans = a*b/a-b;
-        int ans2 = (a*b)/(a-b);
+        int ans = a * b / a - b;
+        int ans2 = (a * b) / (a - b);
         System.out.println(ans);
         System.out.println(ans2);
     }
 
     public static void main(String[] args) {
-        // System.out.println("In this class we can Write some basic code for revision......!!");
-        // takeInput();
-        sumofNumber(10 , 5);
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a :");
+        int a = sc.nextInt();
+        System.out.print("b :");
+        int b = sc.nextInt();
+        sumofNumber(a, b);
+
+
     }
 }
