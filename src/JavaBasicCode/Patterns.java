af6fed31131134baed385259c340ec21ebbf8c5c
Gourav vashistha <gvashistha4@gmail.com>
2024-10-06 02:55:35 +0530

    new patterns

--- src/JavaBasicCode/Patterns.java
+++ src/JavaBasicCode/Patterns.java
@@ -63,8 +63,30 @@ public class Patterns {
         }
     }
 
-    public static void main(String[] args) {
-        TrianglePattern4();
+    public static void TrianglePattern5(){
+        for(int i=1; i<=5;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void  TrianglePattern6(){
+        for(int i = 1;i<=5;i++){
+            for(int j = 1;j<=5-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void FloydsTriangle(){
+
+    }
+
+        public static void main(String[] args) {
+            FloydsTriangle();
 
     }
 }
