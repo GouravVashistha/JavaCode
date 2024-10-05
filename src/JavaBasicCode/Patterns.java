c9dc3a183974ca6afdc3c80d9c578eaa7cef4e60
Gourav vashistha <gvashistha4@gmail.com>
2024-10-06 02:01:27 +0530

    pattern

--- src/JavaBasicCode/Patterns.java
+++ src/JavaBasicCode/Patterns.java
@@ -1,20 +1,21 @@
 package JavaBasicCode;
 
 public class Patterns {
-    public static void squarPattern(){
-        for(int i = 1;i<=4;i++){
-            for(int j=1;j<=5;j++){
+    public static void squarPattern() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
                 System.out.print("*");
             }
-           System.out.println("");
+            System.out.println("");
         }
     }
-    public static void squarPattern2(){
-        for(int i= 1;i<=4;i++){
-            for(int j = 1;j<=5;j++){
-                if(i == 1 || j == 1 || i == 4 || j == 5){
-                     System.out.print("*");
-                }else{
+
+    public static void squarPattern2() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || j == 1 || i == 4 || j == 5) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
             }
@@ -22,9 +23,9 @@ public class Patterns {
         }
     }
 
-    public static void TrianglePattern1(){
-        for(int i = 1;i<=4;i++){
-            for(int j=1;j<=i;j++){
+    public static void TrianglePattern1() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
@@ -32,29 +33,38 @@ public class Patterns {
 
     }
 
-    public static void TrianglePattern2(){
-        for(int i = 1;i<=4;i++){
-            for(int j=i;j<=4;j++){
+    public static void TrianglePattern2() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = i; j <= 4; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
     }
 
-    public static void TrianglePattern3(){
-        for(int i = 4; i>=1;i--){
-            for(int j=1;j<=i;j++){
+    public static void TrianglePattern3() {
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
     }
 
+    public static void TrianglePattern4() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        // squarPattern();
-        // squarPattern2();
-        TrianglePattern3();
+        TrianglePattern4();
 
     }
 }
