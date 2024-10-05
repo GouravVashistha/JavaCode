4d323da916a150ab3cd54bf090f6420c180b3ef2
Gourav vashistha <gvashistha4@gmail.com>
2024-10-06 00:14:16 +0530

    TrianglePattern3

--- src/JavaBasicCode/Patterns.java
+++ src/JavaBasicCode/Patterns.java
@@ -32,10 +32,29 @@ public class Patterns {
 
     }
 
+    public static void TrianglePattern2(){
+        for(int i = 1;i<=4;i++){
+            for(int j=i;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void TrianglePattern3(){
+        for(int i = 4; i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+
     public static void main(String[] args) {
         // squarPattern();
         // squarPattern2();
-        TrianglePattern1();
+        TrianglePattern3();
 
     }
 }
