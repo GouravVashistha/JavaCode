e5261ff999aec458487b8dfc674b71b73b0bc229
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-10-19 00:17:18 +0530

    Create AddingElementInMiddle.java
    middle

---
+++ src/DSA/Arrays/AddingElementInMiddle.java
@@ -0,0 +1,21 @@
+class AddingElementInMiddle{
+public static void main(String[] args) {
+        int[] intArray  = new int[6];
+        int length = 0;
+        for(int i = 1; i<=4; i++){
+            intArray[length] = i;
+            length++;
+        }
+        for(int i = 0;i < intArray.length; i++ ){
+            System.out.println("Index " + i + " contains " + intArray[i]);
+        }
+        System.out.println("---------------------");
+       for(int i =4 ;i>=2;i--){
+           intArray[i+1] = intArray[i];
+       }
+       intArray[2] = 50;
+       for(int i = 0;i < intArray.length; i++ ){
+            System.out.println("Index " + i + " contains " + intArray[i]);
+        }
+    }
+}
