0713b6beeaf3c913aa824f49cb318bef6e0af800
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-10-18 19:48:48 +0530

    Create AddingElementInLast.java
    Array

---
+++ src/DSA/Arrays/AddingElementInLast.java
@@ -0,0 +1,20 @@
+Class AddingElementInLast{
+  public static void main(String[] args) {
+        int[] intArray  = new int[6];
+        int length = 0;
+        for(int i = 0; i<3; i++){
+            intArray[length] = i;
+            length++;
+        }
+        for(int i = 0;i < intArray.length; i++ ){
+            System.out.println("Index " + i + " contains " + intArray[i]);
+        }
+        System.out.println("---------------------");
+        intArray[length] = 10;
+        length++;
+        for(int i = 0;i < intArray.length; i++ ){
+            System.out.println("Index " + i + " contains " + intArray[i]);
+        }
+        System.out.println(length);
+    }
+}
