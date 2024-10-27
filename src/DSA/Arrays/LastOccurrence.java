c32b14fa31f79c6051d022b85fd7563bd7a8c89e
gourav.vashistha <gourav.vashistha@infinite.com>
2024-10-27 20:15:17 +0530

    last occurrence

---
+++ src/DSA/Arrays/LastOccurrence.java
@@ -0,0 +1,19 @@
+package DSA.Arrays;
+
+import java.util.Arrays;
+
+public class LastOccurrence {
+    public static void main(String[] args) {
+        int[] arr = {5,6,5,3,5,4};
+        int value = 5;
+        System.out.println(Arrays.toString(arr));
+        for(int i = arr.length - 1 ; i > 0; i--){
+            if(arr[i]==value){
+                System.out.println("The last occurrence of the "+value+" at the index of : "+i);
+                break;
+            }else{
+                System.out.println("Element not found..");
+            }
+        }
+    }
+}
