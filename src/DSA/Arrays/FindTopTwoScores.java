dcd395030284275220afeaf945538112ec007476
gourav.vashistha <gourav.vashistha@infinite.com>
2024-10-27 21:29:29 +0530

    another approach

--- src/DSA/Arrays/FindTopTwoScores.java
+++ src/DSA/Arrays/FindTopTwoScores.java
@@ -17,10 +17,20 @@ public class FindTopTwoScores {
         return new int[]{firstHigest,secondHigest};
     }
 
+
+    static int[] lastTwoHigest(int[] arr){
+        Arrays.sort(arr);
+        int[] ans = {arr[arr.length-1],arr[arr.length-2]};
+        return ans;
+    }
     public static void main(String[] args) {
         int[] myArray = {84, 85, 86, 87, 85, 90, 85, 83, 23, 45, 84, 1, 2, 0};
         int[] topTwo = FindTopTwoScores.findTopTwo(myArray);
         System.out.println("Array: " + Arrays.toString(myArray));
         System.out.println("First and Second Best Scores: " + Arrays.toString(topTwo));
+        // another way
+        int [] anss = lastTwoHigest(myArray);
+        System.out.println(Arrays.toString(anss));
+       
     }
 }
