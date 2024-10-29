aacc5a3eef83713ab99d750c4fb5383f643a81d1
gourav.vashistha <gourav.vashistha@infinite.com>
2024-10-29 16:10:15 +0530

    reverseArrayUsingSwapping

--- src/DSA/Arrays/RotateTheArray.java
+++ src/DSA/Arrays/RotateTheArray.java
@@ -3,7 +3,7 @@ package DSA.Arrays;
 import java.util.Arrays;
 
 /*
- * Rotate the given array 'a' by k steps, where k is non-negative.
+ * Rotate the given array 'a' by k steps, where k is non-negative. with or without extra space;
  * Note: k can be greater than n as well.
  * 
  * arr = 1,2,3,4,5
@@ -13,7 +13,7 @@ import java.util.Arrays;
  * 
  */
 public class RotateTheArray {
-
+    // with Extra Space
     public static int[] rotateArray(int[] arr ,int k){
         int n = arr.length;
         k = k % n;
@@ -28,9 +28,36 @@ public class RotateTheArray {
         }
         return ans;
     }
+
+    // without Extra space 
+    static void swapInArray(int[] arr, int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void reverseArrayUsingSwapping(int[] arr,int i,int j){
+        while(i<j){
+            swapInArray(arr, i, j);
+            i++;
+            j--;
+        }
+    }
+    public static int[] rotateArraywithout(int[] arr ,int k){
+        int n  = arr.length;
+        k = k % n;
+        reverseArrayUsingSwapping(arr, 0 ,n-k-1);
+        reverseArrayUsingSwapping(arr, n-k ,n-1);
+        reverseArrayUsingSwapping(arr, 0 ,n-1);
+        return arr;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         System.out.println("Original Array: "+ Arrays.toString(arr));
-        System.out.println("Rotate Array  : "+ Arrays.toString(rotateArray(arr, 51)));
+        System.out.println("Rotate Array  : "+ Arrays.toString(rotateArray(arr, 4)));
+
+
+        System.out.println("Original Array: "+ Arrays.toString(arr));
+        System.out.println("Rotate Array  : "+ Arrays.toString(rotateArraywithout(arr,4)));
     }
 }
