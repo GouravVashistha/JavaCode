7d6350d6b2e1f7a5c625590d74e1274bba10e5ca
gourav.vashistha <gourav.vashistha@infinite.com>
2024-10-29 16:17:16 +0530

    update

--- src/DSA/Arrays/RotateTheArray.java
+++ src/DSA/Arrays/RotateTheArray.java
@@ -43,19 +43,20 @@ public class RotateTheArray {
         }
     }
     public static int[] rotateArraywithout(int[] arr ,int k){
-        int n  = arr.length;
-        k = k % n;
-        reverseArrayUsingSwapping(arr, 0 ,n-k-1);
-        reverseArrayUsingSwapping(arr, n-k ,n-1);
-        reverseArrayUsingSwapping(arr, 0 ,n-1);
+        int n  = arr.length; //n = 9
+        k = k % n;          // k = 3
+        reverseArrayUsingSwapping(arr, 0 ,n-k-1); // 0 to (8-4-1)=4-> 5,4,3,2,1,6,7,8
+        reverseArrayUsingSwapping(arr, n-k ,n-1);   //(8-5)=5 to (8-1)->5,4,3,2,1,8,7,6
+        reverseArrayUsingSwapping(arr, 0 ,n-1);   // 0 TO 7 -> 6,8,7,1,2,3,4,5  
         return arr;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,2,3,4,5,6,7,8};
         System.out.println("Original Array: "+ Arrays.toString(arr));
         System.out.println("Rotate Array  : "+ Arrays.toString(rotateArray(arr, 4)));
 
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
         System.out.println("Original Array: "+ Arrays.toString(arr));
         System.out.println("Rotate Array  : "+ Arrays.toString(rotateArraywithout(arr,4)));
