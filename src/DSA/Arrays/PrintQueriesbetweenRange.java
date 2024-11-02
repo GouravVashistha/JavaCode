39c692c4af973f0bca98475efdf5b33eba16084a
GouravVashistha <gvashistha4@gmail.com>
2024-11-03 00:14:40 +0530

    code

--- src/DSA/Arrays/PrintQueriesbetweenRange.java
+++ src/DSA/Arrays/PrintQueriesbetweenRange.java
@@ -1,6 +1,7 @@
 package DSA.Arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /*
  * Given an array of intdgers of size n . Answer q queries where you need to print the sum of valies in a given range
@@ -18,9 +19,29 @@ public class PrintQueriesbetweenRange {
         return sum;
     }
 
+    public static int[] sumofQueriesPrefixSum(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i] + arr[i - 1];
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 2, 4, 1, 3, 6 };
-        int ans = sumofQueries(arr, 2, 5);
-        System.out.println(ans);
+        int[] arr = { 0, 2, 4, 1, 3, 6 };
+        // int ans = sumofQueries(arr, 2, 5);
+        // System.out.println(ans);
+        int[] prefsum = sumofQueriesPrefixSum(arr);
+        System.out.println("Enter number of queries : ");
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+
+        while (q-- > 0) {
+            System.out.println("Enter Range : ");
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int ans = prefsum[r] - prefsum[l - 1];
+            System.out.println("sum " + ans);
+        }
     }
 }
