53e1cf8ac73ef5ffe3d627578d4a5d0a5a55c183
gourav.vashistha <gourav.vashistha@infinite.com>
2024-11-02 13:13:04 +0530

    Update Another Approach

--- src/DSA/Arrays/SortTheArraysZeroOne.java
+++ src/DSA/Arrays/SortTheArraysZeroOne.java
@@ -8,9 +8,11 @@ import java.util.Arrays;
  * ans = 0 0 0 0 0 1 1 1 1
  */
 public class SortTheArraysZeroOne {
+
     static void SortZeroANdOnes(int[] arr) {
         int n = arr.length;
         int count = 0;
+
         // count number of zeros in array
         for (int i = 0; i < n; i++) {
             if (arr[i] == 0) {
@@ -19,19 +21,37 @@ public class SortTheArraysZeroOne {
         }
 
         // 0 to zeroes -1: 0, zeroes to n-1 :1
-        for(int i = 0; i < n ; i++){
-            if(i<count){
+        for (int i = 0; i < n; i++) {
+            if (i < count) {
                 arr[i] = 0;
-            }else{
+            } else {
                 arr[i] = 1;
             }
         }
     }
 
+    // Another approach  more optimized 
+    static void SortZeroANdOnesAnother(int[] arr){
+        int nextZero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                int temp = arr[nextZero];
+                arr[nextZero] = arr[i];
+                arr[i] = temp;
+                nextZero++;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         int arr[] = { 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-        System.out.println("Orignial Arrya : "+Arrays.toString(arr));
-        SortZeroANdOnes(arr);
-        System.out.println("Sorted Arrays : "+Arrays.toString(arr));
+        // System.out.println("Orignial Arrya : " + Arrays.toString(arr));
+        // SortZeroANdOnes(arr);
+        // System.out.println("Sorted Arrays : " + Arrays.toString(arr));
+
+        System.out.println("Orignial Arrya : " + Arrays.toString(arr));
+        SortZeroANdOnesAnother(arr);
+        System.out.println("Sorted Arrays : " + Arrays.toString(arr));
     }
 }
