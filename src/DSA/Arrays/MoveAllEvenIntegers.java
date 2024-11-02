32401b7d034c7c972543fe59376ea4a16cd7eefa
gourav.vashistha <gourav.vashistha@infinite.com>
2024-11-02 16:01:19 +0530

    second app

--- src/DSA/Arrays/MoveAllEvenIntegers.java
+++ src/DSA/Arrays/MoveAllEvenIntegers.java
@@ -35,9 +35,32 @@ public class MoveAllEvenIntegers {
         return ans;
     }
 
+
+    static void sortArrayByParity(int[] arr) {
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        while(left<right) {
+            if(arr[left] % 2 == 1 && arr[right] % 2 == 0) {
+                int temp  = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                
+            }
+            if(arr[left] % 2 == 0) {
+                left++;
+            }
+            if(arr[right] % 2 == 1) {
+                right--;
+            }
+        }
+
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int[] ans = moveAllEvenIntegers(arr);
-        System.out.println(Arrays.toString(ans));
+        // System.out.println(Arrays.toString(ans));
+        sortArrayByParity(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
