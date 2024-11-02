f21f7f06aff51d85c03c49db568a4e0cc58735c4
GouravVashistha <gvashistha4@gmail.com>
2024-11-02 23:51:21 +0530

    Brute Force Approach

--- src/DSA/Arrays/PrintQueriesbetweenRange.java
+++ src/DSA/Arrays/PrintQueriesbetweenRange.java
@@ -1,5 +1,7 @@
 package DSA.Arrays;
 
+import java.util.Arrays;
+
 /*
  * Given an array of intdgers of size n . Answer q queries where you need to print the sum of valies in a given range
  * of indices from 1 to r (both inculded)
@@ -7,7 +9,18 @@ package DSA.Arrays;
  * 
  */
 public class PrintQueriesbetweenRange {
-    public static void main(String[] args) {
+    // Brute Force Approach
+    public static int sumofQueries(int[] arr, int l, int r) {
+        int sum = 0;
+        for (int i = l - 1; i <= r - 1; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 1, 3, 6 };
+        int ans = sumofQueries(arr, 2, 5);
+        System.out.println(ans);
     }
 }
