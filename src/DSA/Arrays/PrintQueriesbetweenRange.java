51d18b3104250c2badf3a93eb056ff8887525957
GouravVashistha <gvashistha4@gmail.com>
2024-11-02 23:37:46 +0530

    queries

---
+++ src/DSA/Arrays/PrintQueriesbetweenRange.java
@@ -0,0 +1,13 @@
+package DSA.Arrays;
+
+/*
+ * Given an array of intdgers of size n . Answer q queries where you need to print the sum of valies in a given range
+ * of indices from 1 to r (both inculded)
+ * Note: The value of 1 and r in queries follow 1-based indexing.
+ * 
+ */
+public class PrintQueriesbetweenRange {
+    public static void main(String[] args) {
+
+    }
+}
