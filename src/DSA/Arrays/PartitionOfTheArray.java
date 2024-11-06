86942d2a72760d1e7d43b7928d10a85b2c4dde46
GouravVashistha <gvashistha4@gmail.com>
2024-11-06 23:36:23 +0530

    unwanted  line remove

--- src/DSA/Arrays/PartitionOfTheArray.java
+++ src/DSA/Arrays/PartitionOfTheArray.java
@@ -35,6 +35,4 @@ public class PartitionOfTheArray {
     public static void main(String[] args) {
         int[] arr = { 1, 3, 5, 6, 2, 1 };
 
-        System.out.println(equalSumPartition(arr)); // Expected output: true
-    }
-}
+        System.out.println(equalSumPartition(arr));
